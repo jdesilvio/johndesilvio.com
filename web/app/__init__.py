a78315d00168181963e693b52c4df2d082c5623c
John DeSilvio <john.desilvio@yahoo.com>
2017-05-21 14:38:24 -0400

    Configured uWSGI

--- web/app/__init__.py
+++ web/app/__init__.py
@@ -10,5 +10,10 @@ def index():
     return render_template('index.html')
 
 
+#def application(env, start_response):
+#    start_response('200 OK', [('Content-Type', 'text/html')])
+#    app.run()
+
+
 if __name__ == '__main__':
     app.run()
